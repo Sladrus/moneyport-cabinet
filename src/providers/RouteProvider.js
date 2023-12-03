b56b7bf41fb9c825ca37e890f8c4cfc8b51d4c1e
Sladrus <ufimkinyasha@gmail.com>
2023-12-03 05:11:08 +0300

    fixes

--- src/providers/RouteProvider.js
+++ src/providers/RouteProvider.js
@@ -1,12 +1,19 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import { RouteContext } from '../context/context';
+import { menuItems } from '../utils/menuItems';
 
 const RouteProvider = ({ children }) => {
   const location = useLocation();
 
   const [selectedMenuItem, setSelectedMenuItem] = useState(1);
   const [selectedSubItem, setSelectedSubItem] = useState();
+
+  useEffect(() => {
+    const route = menuItems.find((item) => item?.path === location?.pathname);
+    console.log(route);
+    if (route) setSelectedMenuItem(route.id);
+  }, [location.pathname]);
 
   const value = {
     selectedMenuItem,

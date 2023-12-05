def17b01797ec89372c131dd1a8595bb030e0b11
Sladrus <ufimkinyasha@gmail.com>
2023-12-05 17:53:40 +0300

    added ym event on go to chat

--- src/utils/window.js
+++ src/utils/window.js
@@ -1,6 +1,7 @@
 import { sendMetric } from './sendMetric';
 
 export const openInNewTab = (url, type) => {
+  sendMetric('reachGoal', 'go_to_chat');
   sendMetric('reachGoal', type);
   window.open(url, '', 'noopener,noreferrer');
 };

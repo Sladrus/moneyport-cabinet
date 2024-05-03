be79f1f3cb01de06d9b29ea1d8c0e657d56eeecd
Яков <yakov.ufimkin@gmail.com>
2024-05-03 10:48:19 +0400

    fix text

--- src/pages/HomePage/HomeContent/index.js
+++ src/pages/HomePage/HomeContent/index.js
@@ -1,60 +1,60 @@
-import React, { useContext } from 'react';
+import React, { useContext } from "react";
 
-import './HomeContent.css';
-import SendTypeButton from '../../../components/Buttons/SendTypeButton';
-import { ReactComponent as BillListIcon } from '../../../assets/icons/sendtypes/bull-list.svg';
-import { ReactComponent as MoneyBagIcon } from '../../../assets/icons/sendtypes/moneybag.svg';
-import { ReactComponent as WalletIcon } from '../../../assets/icons/sendtypes/wallet.svg';
-import { ReactComponent as BitcoinIcon } from '../../../assets/icons/sendtypes/bitcoin.svg';
-import { ReactComponent as UserIcon } from '../../../assets/icons/sendtypes/user.svg';
+import "./HomeContent.css";
+import SendTypeButton from "../../../components/Buttons/SendTypeButton";
+import { ReactComponent as BillListIcon } from "../../../assets/icons/sendtypes/bull-list.svg";
+import { ReactComponent as MoneyBagIcon } from "../../../assets/icons/sendtypes/moneybag.svg";
+import { ReactComponent as WalletIcon } from "../../../assets/icons/sendtypes/wallet.svg";
+import { ReactComponent as BitcoinIcon } from "../../../assets/icons/sendtypes/bitcoin.svg";
+import { ReactComponent as UserIcon } from "../../../assets/icons/sendtypes/user.svg";
 
-import Balances from '../../../components/Balances';
-import History from '../../../components/History';
-import { EXCHANGE_ROUTE, TRANSFERS_ROUTE } from '../../../utils/consts';
-import { useNavigate } from 'react-router-dom';
-import { AuthContext, RouteContext } from '../../../context/context';
-import { sendMetric } from '../../../utils/sendMetric';
+import Balances from "../../../components/Balances";
+import History from "../../../components/History";
+import { EXCHANGE_ROUTE, TRANSFERS_ROUTE } from "../../../utils/consts";
+import { useNavigate } from "react-router-dom";
+import { AuthContext, RouteContext } from "../../../context/context";
+import { sendMetric } from "../../../utils/sendMetric";
 
 const items = [
   {
     id: 2,
-    title: 'Перевод юрлицу',
-    subtitle: 'SWIFT, SEPA',
+    title: "Перевод юрлицу",
+    subtitle: "SWIFT, SEPA",
     Icon: <BillListIcon className="send-type-button-icon" />,
     path: TRANSFERS_ROUTE,
-    type: 'click_perevod_ur',
+    type: "click_perevod_ur",
   },
   {
     id: 3,
-    title: 'Прием из-за рубежа',
-    subtitle: 'Возврат валютной выручки в Россию',
+    title: "Прием из-за рубежа",
+    subtitle: "Возврат валютной выручки в Россию",
     Icon: <MoneyBagIcon className="send-type-button-icon" />,
     path: TRANSFERS_ROUTE,
-    type: 'click_perevod_priem_from_abroad',
+    type: "click_perevod_priem_from_abroad",
   },
   {
     id: 4,
-    title: 'Выдача наличных',
-    subtitle: 'В 35 городах мира',
+    title: "Выдача наличных",
+    subtitle: "В 35 городах мира",
     Icon: <WalletIcon className="send-type-button-icon" />,
     path: TRANSFERS_ROUTE,
-    type: 'click_perevod_cash',
+    type: "click_perevod_cash",
   },
   {
     id: 5,
-    title: 'Обмен криптовалют',
-    subtitle: 'USDT, BTC, ETF и другие',
+    title: "Обмен криптовалют",
+    subtitle: "USDT, BTC, ETH и другие",
     Icon: <BitcoinIcon className="send-type-button-icon" />,
     path: EXCHANGE_ROUTE,
-    type: 'click_cryptoexchange',
+    type: "click_cryptoexchange",
   },
   {
     id: 1,
-    title: 'Перевод физлицу',
-    subtitle: 'PayPal, Zelle, Wise, Revolut, Сбербанк, Тинькофф',
+    title: "Перевод физлицу",
+    subtitle: "PayPal, Zelle, Wise, Revolut, Сбербанк, Тинькофф",
     Icon: <UserIcon className="send-type-button-icon" />,
     path: TRANSFERS_ROUTE,
-    type: 'click_perevod_fiz',
+    type: "click_perevod_fiz",
   },
 ];
 
@@ -66,7 +66,7 @@ const HomeContent = () => {
   const navigate = useNavigate();
 
   const handleClick = (id, title, path, type) => {
-    sendMetric('reachGoal', type);
+    sendMetric("reachGoal", type);
     if (path === TRANSFERS_ROUTE) {
       setSelectedMenuItem(3);
       setSelectedSubItem({ id, title });
